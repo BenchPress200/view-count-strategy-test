9123284962def0f195368e160eab638e48d5ec59
BenchPress200 <jms0538@naver.com>
2026-01-10 20:08:18 +0900

    init

---
+++ src/main/java/com/benchpress200/viewcounter/ViewCounterApplication.java
@@ -0,0 +1,13 @@
+package com.benchpress200.viewcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benchpress200/viewcounter/ViewCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchpress200.viewcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
